a8895ef0be7b7ca45346646fcfc879b9a30bf286
Pedro4064 <pedrohlcruz@gmail.com>
2022-08-28 14:42:06 -0300

    [IMPLEMENTED] Basic Huffman Coding from CSV file

---
+++ Huffman.m
@@ -0,0 +1,28 @@
+% Read the file containing the data
+% file_name = input("Insert the Name of the file with the Data: ", "s");
+data = readtable("test_1.csv"); %! Later we need to add a try catch
+
+% Now we need to order using the probability and add the Node Column
+data = sortrows(data, 'Probability', 'descend');
+data.Nodes = Node.New(data.Probability, data.Symbol);
+
+while height(data) ~= 1
+    % Get the two symbols with lowest probability
+    l_element = data(end, :);
+    s_element = data(end - 1, :);
+
+    % Generate the parent node with the summed probability and names
+    c_prob = l_element{1, end}{1}.probability + s_element{1, end}{1}.probability;
+    c_name = l_element{1, 1}{1} + "+" + s_element{1, 1}{1};
+
+    parent_node = Node(c_prob, c_name, l_element{1, end}{1}, s_element{1, end}{1});
+
+    
+    % Remove the last two nodes 
+    data([end, end-1], :) = [];
+
+    % Add the new node as table entry
+    new_row = parent_node.ToRow();
+    data = [data; new_row];
+    
+end
---
+++ Node.m
@@ -0,0 +1,62 @@
+classdef Node 
+    properties
+        probability
+        symbol_name
+        left_node
+        right_node
+    end
+
+    methods
+        function N = Node(prob, name, left_node, right_node)
+            if nargin == 0
+                N.probability = 0;
+                N.symbol_name = '';
+                N.left_node   = nan;
+                N.right_node   = nan;
+                 
+            elseif nargin == 2
+                 N.probability = prob;
+                 N.symbol_name = name;
+                 N.left_node   = nan;
+                 N.right_node   = nan;
+
+            elseif nargin == 4
+                N.probability = prob;
+                N.symbol_name = name;
+                N.left_node = left_node;
+                N.right_node = right_node;
+             end
+        end
+
+        function row = ToRow(this)
+            row = table();
+            
+            row.Symbol(1) = this.symbol_name;
+            row.Probability(1) = this.probability;
+            row.Nodes(1) = {this};
+        end
+    end
+
+    methods(Static)
+        function N = New(prob, name, left_node, right_node)
+             if isscalar(prob)
+                N = Node(prob, name);
+
+             else
+                 N = cell(height(prob), 1); % Create a cell array by pre-allocating
+                for index = 1:height(prob)
+                    new_node = Node(prob(index), name(index));
+                    N{index, 1} = new_node;
+                    if nargin ~= 2
+                       N{index, 1}.left_node = left_node;
+                       N{index, 1}.right_node = right_node;
+                    end
+                end
+
+                
+            end
+
+        end
+
+     end
+end
