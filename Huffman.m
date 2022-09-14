801e010f4e55f2699332828664a343d685ecb65e
Pedro4064 <pedrohlcruz@gmail.com>
2022-09-14 10:57:20 -0300

    [FIXED] ordering on iteration issue

--- Huffman.m
+++ Huffman.m
@@ -1,5 +1,5 @@
 % Read the file containing the data
-% file_name = input("Insert the Name of the file with the Data: ", "s");
+file_name = input("Insert the Name of the file with the Data: ", "s");
 data = readtable("test_1.csv"); %! Later we need to add a try catch
 
 % Now we need to order using the probability and add the Node Column
@@ -8,8 +8,8 @@ data.Nodes = Node.New(data.Probability, data.Symbol);
 
 while height(data) ~= 1
     % Get the two symbols with lowest probability
-    l_element = data(end, :);
-    s_element = data(end - 1, :);
+    l_element = data(end - 1, :);
+    s_element = data(end, :);
 
     % Generate the parent node with the summed probability and names
     c_prob = l_element{1, end}{1}.probability + s_element{1, end}{1}.probability;
@@ -21,8 +21,9 @@ while height(data) ~= 1
     % Remove the last two nodes 
     data([end, end-1], :) = [];
 
-    % Add the new node as table entry
+    % Add the new node as table entry and order it
     new_row = parent_node.ToRow();
     data = [data; new_row];
+    data = sortrows(data, 'Probability', 'descend')
     
 end
